5f78ac7adc0fb716bdc132edd63d4557594d3a74
leonluz80 <leon.luz002@gmail.com>
2026-04-13 10:28:14 +0200

    revert 2nd last commit

--- src/app/application/config/battery_system_cfg.h
+++ src/app/application/config/battery_system_cfg.h
@@ -86,7 +86,7 @@ typedef enum {
 } BS_STRING_ID_e;
 
 /** Define if discharge current is seen as positive or negative */
-#define BS_POSITIVE_DISCHARGE_CURRENT (false)
+#define BS_POSITIVE_DISCHARGE_CURRENT (true)
 
 /**
  * @brief   Number of parallel strings in the battery pack
